3ecc40fb7e0fe438b149ca73ea9ab497c587f747
Eun Jung Choi <im.your.energy@gmail.com>
2020-08-19 11:55:41 +0900

    add 11. container with most water

---
+++ leetcode/most_liked/11_container_with_most_water.py
@@ -0,0 +1,17 @@
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        last = len(height) - 1
+        for i, item in enumerate(height):
+            key = i + 1
+            while key <= last:
+                h = height[i] if height[i] <= height[key] else height[key]
+                w = key - i
+                area = w * h
+                if area > max_area:
+                    max_area = area
+                key += 1
+
+        return max_area
