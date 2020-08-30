417b5b88bdb4b6e739dbe3105a7257b98a4e4aa9
Eun Jung Choi <im.your.energy@gmail.com>
2020-08-30 21:04:02 +0900

    Add 316. remove duplicate letters (by stack)

--- leetcode/most_liked/316_remove_duplicate_letters.py
+++ leetcode/most_liked/316_remove_duplicate_letters.py
@@ -9,6 +9,10 @@
 # Input: "cbacdcbc"
 # Output: "acdb"
 
+# by recursion
+import collections
+
+
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         # 집합으로 만들어 중복 없애고 정렬
@@ -20,6 +24,49 @@ class Solution:
                 new_s = suffix.replace(i, '')
                 return i + self.removeDuplicateLetters(new_s)
         return ''
+
+
+# 286 / 286 test cases passed.
+# Status: Accepted
+# Runtime: 56 ms
+# Memory Usage: 13.9 MB
+#
+# Your runtime beats 34.22 % of python3 submissions.
+# Your memory usage beats 43.38 % of python3 submissions.
+
+
+# by stack
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        stack = []
+        seen = set()  # 스택과 동일하게 저장함. 검색용.
+        counter = collections.Counter(s)
+
+        for i in s:
+            counter[i] -= 1
+
+            if i in seen:
+                continue
+
+            # 스택의 top과 비교해 현재 아이템이 top보다 앞서고, top의 개수가 아직 남아있으면  stack에서 top을 제
+            while stack and i < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+
+            stack.append(i)
+            seen.add(i)
+
+        return ''.join(stack)
+
+
+# by stack:
+# 286 / 286 test cases passed.
+# Status: Accepted
+# Runtime: 32 ms
+# Memory Usage: 13.9 MB
+#
+# Your runtime beats 92.15 % of python3 submissions.
+# Your memory usage beats 48.15 % of python3 submissions.
+
 
 
 
