99b3a2d532f8340c793baab181c6329ba9b9cdcb
Eun Jung Choi <im.your.energy@gmail.com>
2020-09-01 00:39:25 +0900

    Add data structure: implement queue using stacks

---
+++ data_structure/232_implement_queue_using_stacks.py
@@ -0,0 +1,82 @@
+# Implement the following operations of a queue using stacks.
+#
+# push(x) -- Push element x to the back of queue.
+# pop() -- Removes the element from in front of queue.
+# peek() -- Get the front element.
+# empty() -- Return whether the queue is empty.
+# Example:
+#
+# MyQueue queue = new MyQueue();
+#
+# queue.push(1);
+# queue.push(2);
+# queue.peek();  // returns 1
+# queue.pop();   // returns 1
+# queue.empty(); // returns false
+# Notes:
+#
+# You must use only standard operations of a stack --
+# which means only push to top, peek/pop from top, size, and is empty operations are valid.
+# Depending on your language, stack may not be supported natively.
+# You may simulate a stack by using a list or deque (double-ended queue),
+# as long as you use only standard operations of a stack.
+# You may assume that all operations are valid
+# (for example, no pop or peek operations will be called on an empty queue).
+
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.peek()
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        # stack을 그대로 엎어서 다른 통에 넣는 식. 
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return len(self.stack1) == 0 and len(self.stack2) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
+
+# 17 / 17 test cases passed.
+# Status: Accepted
+# Runtime: 24 ms
+# Memory Usage: 13.9 MB
+#
+# Your runtime beats 93.77 % of python3 submissions.
+# Your memory usage beats 31.35 % of python3 submissions.
+
