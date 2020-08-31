7a4df0b9605095cecd10770b6be824964ff14d0c
Eun Jung Choi <im.your.energy@gmail.com>
2020-08-31 23:41:18 +0900

    Add data structure : implement stack using queues

---
+++ data_structure/225_implement_stack_using_queues.py
@@ -0,0 +1,67 @@
+# Implement the following operations of a stack using queues.
+#
+# push(x) -- Push element x onto stack.
+# pop() -- Removes the element on top of the stack.
+# top() -- Get the top element.
+# empty() -- Return whether the stack is empty.
+# Example:
+#
+# MyStack stack = new MyStack();
+#
+# stack.push(1);
+# stack.push(2);
+# stack.top();   // returns 2
+# stack.pop();   // returns 2
+# stack.empty(); // returns false
+import collections
+
+
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q = collections.deque()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.q.append(x)
+        for i in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.q.popleft()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.q[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return len(self.q) == 0
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+
+
+# 16 / 16 test cases passed.
+# Status: Accepted
+# Runtime: 20 ms
+# Memory Usage: 13.9 MB
+#
+# Your runtime beats 98.90 % of python3 submissions.
+# Your memory usage beats 41.62 % of python3 submissions.
