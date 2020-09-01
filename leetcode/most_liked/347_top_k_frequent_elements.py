51e8333e77852200db9f751fdb2b97baaf5fd8a7
Eun Jung Choi <im.your.energy@gmail.com>
2020-09-02 03:49:19 +0900

    Add 347. top k frequent (2) : using zip and heap

--- leetcode/most_liked/347_top_k_frequent_elements.py
+++ leetcode/most_liked/347_top_k_frequent_elements.py
@@ -20,7 +20,22 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [value for value, _ in collections.Counter(nums).most_common(k)]
+        # return [value for value, _ in collections.Counter(nums).most_common(k)]
+
+        # pythonic way: zip()의 결과는 제너레이터를 반환. 실제값을 추출하기 위해 list()로 한번더 묶어 줌
+        return list(zip(*collections.Counter(nums).most_common(k)))[0]
+
+        # using heap
+        # freqs = collections.Counter(nums)
+        # heap = []
+        # for f in freqs:
+        #     heapq.heappush(heap, (-freqs[f], f))
+        #
+        # result = []
+        # for _ in range(k):
+        #     result.append(heapq.heappop(heap)[1])
+        #
+        # return result
 
 
 # 21 / 21 test cases passed.
@@ -30,3 +45,12 @@ class Solution:
 #
 # Your runtime beats 30.63 % of python3 submissions.
 # Your memory usage beats 75.91 % of python3 submissions.
+
+
+# using zip()
+# Your runtime beats 95.64 % of python3 submissions.
+# Your memory usage beats 71.80 % of python3 submissions.
+
+# using heap
+# Your runtime beats 98.63 % of python3 submissions.
+# Your memory usage beats 26.08 % of python3 submissions.
