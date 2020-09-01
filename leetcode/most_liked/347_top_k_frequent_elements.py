6adb46ef7060d0552f145c1b0c1e5a909dfa9bab
Eun Jung Choi <im.your.energy@gmail.com>
2020-09-02 03:13:06 +0900

    Add 347. top K frequent elements using collections.Counter

---
+++ leetcode/most_liked/347_top_k_frequent_elements.py
@@ -0,0 +1,32 @@
+# Given a non-empty array of integers, return the k most frequent elements.
+#
+# Example 1:
+#
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+# Example 2:
+#
+# Input: nums = [1], k = 1
+# Output: [1]
+# Note:
+#
+# You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+# Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+# It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+# You can return the answer in any order.
+import collections
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return [value for value, _ in collections.Counter(nums).most_common(k)]
+
+
+# 21 / 21 test cases passed.
+# Status: Accepted
+# Runtime: 156 ms
+# Memory Usage: 18.5 MB
+#
+# Your runtime beats 30.63 % of python3 submissions.
+# Your memory usage beats 75.91 % of python3 submissions.
