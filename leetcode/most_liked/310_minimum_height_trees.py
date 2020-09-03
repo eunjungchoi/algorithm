bfcda9074795d8f2bae4bb17080944cdaf45ae1b
Eun Jung Choi <im.your.energy@gmail.com>
2020-09-04 06:04:00 +0900

    Add 310. minimum height trees (iterative)

---
+++ leetcode/most_liked/310_minimum_height_trees.py
@@ -0,0 +1,80 @@
+# A tree is an undirected graph in which any two vertices are connected by exactly one path. In other words, any connected graph without simple cycles is a tree.
+#
+# Given a tree of n nodes and n - 1 edges where edges[i] = [ai, bi] indicates that there is an undirected edge between the two nodes ai and bi in the tree, you can choose any node of the tree as the root. When you select a node x as the root, the result tree has height h. Among all possible rooted trees, those with minimum height (i.e. min(h))  are called minimum height trees (MHTs).
+#
+# Return a list of the root labels of all the MHTs. You can return the answer in any order.
+#
+# The height of a rooted tree is the number of edges on the longest downward path between the root and a leaf.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: n = 4, edges = [[1,0],[1,2],[1,3]]
+# Output: [1]
+# Explanation: As shown, the height of the tree is 1 when the root is one which is the only MHT.
+# Example 2:
+#
+#
+# Input: n = 6, edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
+# Output: [3,4]
+# Example 3:
+#
+# Input: n = 1, edges = []
+# Output: [0]
+# Example 4:
+#
+# Input: n = 2, edges = [[0,1]]
+# Output: [0,1]
+#
+#
+# Constraints:
+#
+# 1 <= n <= 2 * 104
+# edges.length == n - 1
+# 0 <= ai, bi < n
+# ai != bi
+# All the pairs (ai, bi) are distinct.
+# The given input is guaranteed to be a tree
+import collections
+from typing import List
+
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n <= 1:
+            return [0]
+
+        # 양방향 그래프 구성
+        graph = collections.defaultdict(list)
+        for i, j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+
+        # 첫번째 리프 노드 추가
+        leaves = []
+        for i in range(n + 1):
+            if len(graph[i]) == 1:
+                leaves.append(i)
+
+        # 루트 노드만 남을 때까지 반복 제거
+        while n > 2:
+            n -= len(leaves)
+            new_leaves = []
+            for leaf in leaves:
+                neighbor = graph[leaf].pop()
+                graph[neighbor].remove(leaf)
+
+                if len(graph[neighbor]) == 1:
+                    new_leaves.append(neighbor)
+
+            leaves = new_leaves
+
+        return leaves
+
+
+# Runtime: 244 ms, faster than 94.26% of Python3 online submissions for Minimum Height Trees.
+# Memory Usage: 17.5 MB, less than 94.54% of Python3 online submissions for Minimum Height Trees.
+# <파이썬 알고리즘 인터뷰> 참고.
+
