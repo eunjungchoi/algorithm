f5bafed162b98798c7d406317910810c00c5493d
Eun Jung Choi <im.your.energy@gmail.com>
2020-09-08 03:51:26 +0900

    Add sort algorithm: bubble sort

---
+++ sort_algorithm/bubble_sort.py
@@ -0,0 +1,8 @@
+
+def bubble_sort(A):
+    for i in range(1, len(A)):
+        for j in range(0, len(A)-1):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+
+# 시간 복잡도:  O(n^2)
