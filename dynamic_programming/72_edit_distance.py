a54eec0a1a435e79fea250911ce50802ac7c2790
Eun Jung Choi <im.your.energy@gmail.com>
2020-09-18 23:09:39 +0900

    Add 72. edit distance (naive recursive)

--- dynamic_programming/72_edit_distance.py
+++ dynamic_programming/72_edit_distance.py
@@ -27,6 +27,23 @@
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+
+        """Naive recursive solution ==> timeout error """
+        # if not word1 and not word2:
+        #     return 0
+        # if not word1:
+        #     return len(word2)
+        # if not word2:
+        #     return len(word1)
+        #
+        # if word1[0] == word2[0]:
+        #     return self.minDistance(word1[1:], word2[1:])
+        #
+        # insert = 1 + self.minDistance(word1, word2[1:])
+        # delete = 1 + self.minDistance(word1[1:], word2)
+        # replace = 1 + self.minDistance(word1[1:], word2[1:])
+        # return min(insert, replace, delete)
+
         """Dynamic programming solution"""
         m = len(word1)
         n = len(word2)
