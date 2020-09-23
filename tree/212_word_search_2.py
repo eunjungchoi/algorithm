9faf77da5ed261bb36bb11f3693fbf7827d1cc85
Eun Jung Choi <im.your.energy@gmail.com>
2020-09-24 00:00:02 +0900

    Add 212. word search 2 (trie, tree, DFS)

---
+++ tree/212_word_search_2.py
@@ -0,0 +1,86 @@
+# Given a 2D board and a list of words from the dictionary, find all words in the board.
+#
+# Each word must be constructed from letters of sequentially adjacent cell,
+# where "adjacent" cells are those horizontally or vertically neighboring.
+# The same letter cell may not be used more than once in a word.
+#
+#
+#
+# Example:
+#
+# Input:
+# board = [
+#   ['o','a','a','n'],
+#   ['e','t','a','e'],
+#   ['i','h','k','r'],
+#   ['i','f','l','v']
+# ]
+# words = ["oath","pea","eat","rain"]
+#
+# Output: ["eat","oath"]
+#
+#
+# Note:
+#
+# All inputs are consist of lowercase letters a-z.
+# The values of words are distinct.
+
+
+class TrieNode():
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.isWord = False
+
+
+class Trie():
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.isWord = True
+
+    def search(self, word):
+        node = self.root
+        for w in word:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return node.isWord
+
+
+class Solution:
+    def findWords(self, board, words):
+        res = []
+        trie = Trie()
+        node = trie.root
+        for w in words:
+            trie.insert(w)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                self.dfs(board, node, i, j, "", res)
+        return res
+
+    def dfs(self, board, node, i, j, path, res):
+        if node.isWord:
+            res.append(path)
+            node.isWord = False
+
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+            return
+
+        tmp = board[i][j]
+        node = node.children.get(tmp)
+
+        if not node:
+            return
+
+        board[i][j] = "#"
+        self.dfs(board, node, i + 1, j, path + tmp, res)
+        self.dfs(board, node, i - 1, j, path + tmp, res)
+        self.dfs(board, node, i, j - 1, path + tmp, res)
+        self.dfs(board, node, i, j + 1, path + tmp, res)
+        board[i][j] = tmp
+
