d90b405e81c0abfabbc3774949a79f42e9657124
Eun Jung Choi <im.your.energy@gmail.com>
2020-09-24 16:00:12 +0900

    Add 146. LRU cache (hashmap, double linked list)

--- hashtable/146_LRU_cache.py
+++ hashtable/146_LRU_cache.py
@@ -70,3 +70,88 @@ class LRUCache(OrderedDict):
 # param_1 = obj.get(key)
 # obj.put(key,value)
 
+
+class DoubleLinkedNode():
+    def __init__(self):
+        self.key = 0
+        self.value = 0
+        self.prev = None
+        self.next = None
+
+
+class LRUCache2():  # hashmap + double linked list
+    def _add_node(self, node):
+        """
+        Always add the new node right after head.
+        """
+        node.prev = self.head
+        node.next = self.head.next
+
+        self.head.next.prev = node
+        self.head.next = node
+
+    def _remove_node(self, node):
+        """
+        Remove an exising node from the linked list.
+        """
+        prev = node.prev
+        new = node.next
+
+        prev.next = new
+        new.prev = prev
+
+    def _move_to_head(self, node):
+        """
+        Move certain node in between to the head
+        """
+        self._remove_node(node)
+        self._add_node(node)
+
+    def _pop_tail(self):
+        """
+        Pop the current tail.
+        """
+        res = self.tail.prev
+        self._remove_node(res)
+        return res
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.size = 0
+        self.capacity = capacity
+        self.head, self.tail = DoubleLinkedNode(), DoubleLinkedNode()
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        node = self.cache.get(key, None)
+        if not node:
+            return -1
+
+        # move the accessed node to the head
+        self._move_to_head(node)
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        node = self.cache.get(key)
+
+        if not node:
+            new_node = DoubleLinkedNode()
+            new_node.key = key
+            new_node.value = value
+
+            self.cache[key] = new_node
+            self._add_node(new_node)
+            self.size += 1
+
+            if self.size > self.capacity:
+                # pop the tail
+                tail = self._pop_tail()
+                del self.cache[tail.key]
+                self.size -= 1
+
+        else:
+            # update the value
+            node.value = value
+            self._move_to_head(node)
