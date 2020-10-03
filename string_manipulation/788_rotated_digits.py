4102db16be175f138fbd0e5dff926d851f1da1f5
Eun Jung Choi <im.your.energy@gmail.com>
2020-10-03 21:18:01 +0900

    Add 788. rotated digits (dynamic programming)

--- string_manipulation/788_rotated_digits.py
+++ string_manipulation/788_rotated_digits.py
@@ -53,3 +53,59 @@ class Solution:
 # Memory Usage: 14.1 MB
 # Your runtime beats 40.89 % of python3 submissions.
 # Your memory usage beats 16.72 % of python3 submissions.
+
+    def rotatedDigits1(self, N):
+        s1 = set([0, 1, 8])
+        s2 = set([0, 1, 8, 2, 5, 6, 9])
+        s = set()
+        res = 0
+        N = list(map(int, str(N)))
+        for i, v in enumerate(N):
+            for j in range(v):
+                if s.issubset(s2) and j in s2:
+                    res += 7 ** (len(N) - i - 1)
+                if s.issubset(s1) and j in s1:
+                    res -= 3 ** (len(N) - i - 1)
+            if v not in s2:
+                return res
+            s.add(v)
+        return res + (s.issubset(s2) and not s.issubset(s1))
+
+    def rotatedDigits2(self, N: int) -> int:
+        # dynamic programming on digits
+        # 2, 5, 6, 9 중에 하나는 써야 한다.
+        # i: 얼마나 많은 숫자를 썼는지
+        # equality_flag: N의 j번째 숫자보다 작은 j 번째 숫자를 썼는지
+        # involution_flag: 2569 중 하나를 썼는지
+
+        # dp(i, equality_flag, invoution_flag) 는 위의 조건에 해당하는 N의 접미사를 쓰는 방법의 수. 우리가 원하는 답은 dp(0, True, False) 이다.
+
+        # 알고리즘:
+        # equality_flag가 참일 경우, i번째 digit은 최대 N의 i번째 수가 된다. 각 숫자 d에 대해 현재 설정된 flag를 기준으로 d를 쓸 수 있는지 판단한다.
+
+        A = list(map(int, str(N)))
+        memo = {}
+
+        def dp(i, equality_flag, involution_flag):
+            if i == len(A):
+                return +involution_flag
+
+            if (i, equality_flag, involution_flag) not in memo:
+                result = 0
+                for d in range(A[i] + 1 if equality_flag else 10):
+                    if d in {3, 4, 7}: continue
+                    result += dp(i + 1, equality_flag and d == A[i], involution_flag or d in {2, 5, 6, 9})
+
+                memo[i, equality_flag, involution_flag] = result
+
+            return memo[i, equality_flag, involution_flag]
+
+        return dp(0, True, False)
+
+
+# 50 / 50 test cases passed.
+# Status: Accepted
+# Runtime: 24 ms
+# Memory Usage: 14.3 MB
+#
+# Your runtime beats 99.70 % of python3 submissions.
