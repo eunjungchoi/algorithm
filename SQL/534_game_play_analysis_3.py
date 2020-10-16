37ebab64174b32a63f8000e6342a4ac2f482e4ca
Eun Jung Choi <im.your.energy@gmail.com>
2020-10-16 22:52:31 +0900

    Add sql: 532. game play (inner join)

--- SQL/534_game_play_analysis_3.py
+++ SQL/534_game_play_analysis_3.py
@@ -50,3 +50,9 @@
 # where a.player_id = b.player_id and a.event_date >= b.event_date
 # group by a.player_id, a.event_date
 
+
+# select a1.player_id, a1.event_date, sum(a2.games_played) as games_played_so_far
+# from activity as a1
+# inner join activity as a2
+# on a1.player_id = a2.player_id and a1.event_date >= a2.event_date
+# group by  a1.player_id, a1.event_date
