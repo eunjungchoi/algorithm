b831c84102fb79d021ee263c0c7afa5422c300bc
Eun Jung Choi <im.your.energy@gmail.com>
2020-10-17 17:27:00 +0900

    Add sql: 1112. highest grade for each student (sub query, min, max, group by)

---
+++ SQL/1112_highest_grade_for_each_student.py
@@ -0,0 +1,49 @@
+# Table: Enrollments
+#
+# +---------------+---------+
+# | Column Name   | Type    |
+# +---------------+---------+
+# | student_id    | int     |
+# | course_id     | int     |
+# | grade         | int     |
+# +---------------+---------+
+# (student_id, course_id) is the primary key of this table.
+#
+# Write a SQL query to find the highest grade with its corresponding course for each student.
+# In case of a tie, you should find the course with the smallest course_id. The output must be sorted by increasing student_id.
+#
+# The query result format is in the following example:
+#
+# Enrollments table:
+# +------------+-------------------+
+# | student_id | course_id | grade |
+# +------------+-----------+-------+
+# | 2          | 2         | 95    |
+# | 2          | 3         | 95    |
+# | 1          | 1         | 90    |
+# | 1          | 2         | 99    |
+# | 3          | 1         | 80    |
+# | 3          | 2         | 75    |
+# | 3          | 3         | 82    |
+# +------------+-----------+-------+
+#
+# Result table:
+# +------------+-------------------+
+# | student_id | course_id | grade |
+# +------------+-----------+-------+
+# | 1          | 2         | 99    |
+# | 2          | 2         | 95    |
+# | 3          | 3         | 82    |
+# +------------+-----------+-------+
+
+
+# select student_id, min(course_id) as course_id, grade
+# from enrollments
+# where (student_id, grade) in
+# (
+#     select student_id, max(grade)
+#     from enrollments
+#     group by student_id
+# )
+# group by student_id, grade
+# order by student_id asc
